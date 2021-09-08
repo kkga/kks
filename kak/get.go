93e09850b42680ca3a89863362e5746d80357e6b
Gadzhi Kharkharov <me@kkga.me>
2021-09-09 02:37:03 +0300

    rewrite Get to use fsnotify for reading

--- kak/get.go
+++ kak/get.go
@@ -2,12 +2,15 @@ package kak
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"strings"
-	"time"
+
+	"github.com/fsnotify/fsnotify"
 )
 
 func Get(getStr, session, client string) ([]string, error) {
+	// create a tmp file where kak echo the value
 	f, err := os.CreateTemp("", "kks-tmp")
 	if err != nil {
 		return nil, err
@@ -15,23 +18,63 @@ func Get(getStr, session, client string) ([]string, error) {
 	defer os.Remove(f.Name())
 	defer f.Close()
 
-	sendCmd := fmt.Sprintf("echo -quoting kakoune -to-file %s %%{ %s }", f.Name(), getStr)
+	// kak will output to file, so we create a chan for reading
+	ch := make(chan string)
+	go readTmp(f, ch)
 
+	// tell kak to echo the requested state
+	sendCmd := fmt.Sprintf("echo -quoting kakoune -to-file %s %%{ %s }", f.Name(), getStr)
 	if err := Send(sendCmd, "", session, client); err != nil {
 		return nil, err
 	}
-	// TODO: need to wait for Send to finish
-	time.Sleep(10 * time.Millisecond)
 
-	out, err := os.ReadFile(f.Name())
-	if err != nil {
-		return nil, err
-	}
+	// wait until tmp file is populated and read
+	output := <-ch
 
-	outStrs := strings.Split(string(out), " ")
+	// trim kakoune quoting from output
+	outStrs := strings.Split(output, " ")
 	for i, val := range outStrs {
 		outStrs[i] = strings.Trim(val, "''")
 	}
 
 	return outStrs, nil
+}
+
+func readTmp(f *os.File, c chan string) {
+	// create a watcher
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	// add file to watch
+	err = watcher.Add(f.Name())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// while we don't get the value
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok {
+				return
+			}
+			// if file written, read it and send to chan
+			if event.Op&fsnotify.Write == fsnotify.Write {
+				dat, err := os.ReadFile(f.Name())
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				c <- string(dat)
+			}
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				return
+			}
+			log.Println("error:", err)
+		}
+	}
 }
