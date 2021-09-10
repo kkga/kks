120b18837aee6f04b148060c31c0f77268cc21d9
Gadzhi Kharkharov <me@kkga.me>
2021-09-10 08:12:25 +0300

    use pipe for send

--- kak/send.go
+++ kak/send.go
@@ -2,30 +2,43 @@ package kak
 
 import (
 	"fmt"
-	"os"
+	"io"
 	"os/exec"
-	"strings"
 )
 
 func Send(kakCommand, buffer, session, client string) error {
 	cmd := exec.Command("kak", "-p", session)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	// cmd.Stdout = os.Stdout
+	// cmd.Stderr = os.Stderr
 
-	var in strings.Builder
-	in.WriteString("evaluate-commands")
-	if buffer != "" {
-		in.WriteString(fmt.Sprintf(" -buffer %s", buffer))
-	} else if client != "" {
-		in.WriteString(fmt.Sprintf(" -try-client %s", client))
-	}
-	in.WriteString(fmt.Sprintf(" %s", kakCommand))
+	stdin, err := cmd.StdinPipe()
 
-	cmd.Stdin = strings.NewReader(in.String())
+	go func() {
+		defer stdin.Close()
+		io.WriteString(stdin, "evaluate-commands")
+		if buffer != "" {
+			io.WriteString(stdin, fmt.Sprintf(" -buffer %s", buffer))
+		} else if client != "" {
+			io.WriteString(stdin, fmt.Sprintf(" -try-client %s", client))
+		}
+		io.WriteString(stdin, fmt.Sprintf(" %s", kakCommand))
+	}()
 
-	err := cmd.Run()
+	// var in strings.Builder
+	// in.WriteString("evaluate-commands")
+	// if buffer != "" {
+	// 	in.WriteString(fmt.Sprintf(" -buffer %s", buffer))
+	// } else if client != "" {
+	// 	in.WriteString(fmt.Sprintf(" -try-client %s", client))
+	// }
+	// in.WriteString(fmt.Sprintf(" %s", kakCommand))
+
+	// cmd.Stdin = strings.NewReader(in.String())
+
+	_, err = cmd.CombinedOutput()
 	if err != nil {
 		return err
 	}
+	// fmt.Printf("%s\n", out)
 	return nil
 }
