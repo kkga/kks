90cfe35fb3b100227cfeca8a40e64e27bba73aa6
Gadzhi Kharkharov <me@kkga.me>
2021-09-10 18:10:38 +0300

    add buffer to context

--- kak/context.go
+++ kak/context.go
@@ -13,9 +13,9 @@ type KakContext struct {
 	Session string `json:"session"`
 	Client  string `json:"client"`
 	WorkDir string `json:"workdir"`
+	Buffer  string `json:"buffer"`
 }
 
-// TODO: add args for custom session, client here
 func NewContext(sess, cl string) (*KakContext, error) {
 	kc := KakContext{
 		Session: os.Getenv("KKS_SESSION"),
@@ -29,20 +29,16 @@ func NewContext(sess, cl string) (*KakContext, error) {
 		kc.Client = cl
 	}
 
-	dir, err := Get("%sh{pwd}", "", kc)
-	if err == nil {
+	dir, _ := Get("%sh{pwd}", "", kc)
+	if dir != nil {
 		kc.WorkDir = dir[0]
 	}
 
-	// if session != "" {
-	// 	c.Session = session
-	// }
-	// if client != "" {
-	// 	c.Client = client
-	// }
-	// if c.Session == "" {
-	// 	return nil, errors.New("No session in context")
-	// }
+	buf, _ := Get("%val{bufname}", "", kc)
+	if buf != nil {
+		kc.Buffer = buf[0]
+	}
+
 	return &kc, nil
 }
 
@@ -50,7 +46,15 @@ func (k *KakContext) Exists() error {
 	if k.Session == "" {
 		return errors.New("No session in context")
 	}
-	return nil
+
+	kakSessions, _ := List()
+	for _, sess := range kakSessions {
+		if sess.Name != k.Session {
+			return nil
+		}
+	}
+
+	return errors.New(fmt.Sprintf("Session doesn't exist: %s", k.Session))
 }
 
 func (k *KakContext) Print(jsonOutput bool) {
@@ -65,5 +69,6 @@ func (k *KakContext) Print(jsonOutput bool) {
 		fmt.Printf("session: %s\n", k.Session)
 		fmt.Printf("client: %s\n", k.Client)
 		fmt.Printf("workdir: %s\n", k.WorkDir)
+		fmt.Printf("buffer: %s\n", k.Buffer)
 	}
 }
