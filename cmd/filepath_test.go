74fc960198f0f3ed18a6f51a70194e08da3c2d48
Gadzhi Kharkharov <me@kkga.me>
2021-09-12 12:08:21 +0300

    fix filepath tests

--- cmd/filepath_test.go
+++ cmd/filepath_test.go
@@ -9,39 +9,35 @@ import (
 // TODO: add tests for resolving paths
 func TestNewFilepath(t *testing.T) {
 	tests := []struct {
-		args  []string
-		cwd   string
-		kakwd string
-		want  Filepath
+		args []string
+		want Filepath
 	}{
 		{
 			[]string{"file"},
-			"/home/k/p/kks/",
-			"/home/k/p/kks/",
-			Filepath{Name: "file", Raw: []string{"file"}},
+			Filepath{Name: "/home/kkga/projects/kks/cmd/file",
+				Raw: []string{"file"}},
 		},
 		{
-			[]string{"file.kak", "+22"},
-			"/home/k/p/",
-			"/home/k/p/kks/",
-			Filepath{Name: "file.kak", Line: 22, Raw: []string{"file.kak", "+22"}},
+			[]string{"../file.kak", "+22"},
+			Filepath{Name: "/home/kkga/projects/kks/file.kak",
+				Line: 22,
+				Raw:  []string{"../file.kak", "+22"}},
 		},
 		{
-			[]string{"readme", "+10:2"},
-			"/home/k/p/kks/",
-			"/home/k/p/kks/",
-			Filepath{Name: "readme", Line: 10, Column: 2, Raw: []string{"readme", "+10:2"}},
+			[]string{"/etc/readme", "+10:2"},
+			Filepath{Name: "/etc/readme",
+				Line: 10, Column: 2,
+				Raw: []string{"/etc/readme", "+10:2"}},
 		},
 		{
-			[]string{"readme", ":2"},
-			"/home/k/p/kks/",
-			"/home/k/p/kks/",
-			Filepath{Name: "readme", Raw: []string{"readme", ":2"}},
+			[]string{"../../../downloads/readme", ":2"},
+			Filepath{Name: "/home/kkga/downloads/readme",
+				Raw: []string{"../../../downloads/readme", ":2"}},
 		},
 	}
 	for _, tt := range tests {
 		t.Run("", func(t *testing.T) {
-			got, err := NewFilepath(tt.args, "", "")
+			got, err := NewFilepath(tt.args)
 			if err != nil {
 				t.Fatal("can't get Filepath: ", err)
 			}
