b0671eef19355bc3e9c03026ad436fb959977c81
Gadzhi Kharkharov <me@kkga.me>
2021-09-16 00:15:38 +0300

    add version flag

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/kkga/kks/cmd"
 )
 
+var version = "dev"
+
 func main() {
 	log.SetFlags(0)
+
+	if len(os.Args) > 1 && os.Args[1] == "-v" {
+		fmt.Printf("kks %s\n", version)
+		os.Exit(0)
+	}
+
 	if err := cmd.Root(os.Args[1:]); err != nil {
 		log.Fatal(err)
 	}
