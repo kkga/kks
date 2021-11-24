f9f5fb459d8acf433c0bdf9df2040c8302010978
Gadzhi Kharkharov <me@kkga.me>
2021-11-24 22:22:30 +0300

    write kakoune errors to stdout in get command

--- cmd/get.go
+++ cmd/get.go
@@ -29,6 +29,14 @@ type GetCmd struct {
 	raw bool
 }
 
+type kakErr struct {
+	err string
+}
+
+func (e *kakErr) Error() string {
+	return fmt.Sprintf("kak_error: %s", e.err)
+}
+
 func (c *GetCmd) Run() error {
 	query := c.fs.Arg(0)
 	if query == "" {
@@ -39,6 +47,12 @@ func (c *GetCmd) Run() error {
 	resp, err := kak.Get(c.kctx, query)
 	if err != nil {
 		return err
+	}
+
+	if strings.HasPrefix(resp, "__kak_error__") {
+		kakOutErr := strings.TrimPrefix(resp, "__kak_error__")
+		kakOutErr = strings.TrimSpace(kakOutErr)
+		return &kakErr{kakOutErr}
 	}
 
 	if c.raw {
--- kak/send.go
+++ kak/send.go
@@ -3,30 +3,49 @@ package kak
 import (
 	"fmt"
 	"io"
+	"os"
 	"os/exec"
 )
 
-func Send(kctx *Context, command string) error {
+func Send(kctx *Context, kakCommand string, errOutFile *os.File) error {
 	kakExec, err := kakExec()
 	if err != nil {
 		return err
 	}
 	cmd := exec.Command(kakExec, "-p", kctx.Session.Name)
-	// cmd.Stdout = os.Stdout
-	// cmd.Stderr = os.Stderr
 
 	stdin, err := cmd.StdinPipe()
 
 	go func() {
-		io.WriteString(stdin, "evaluate-commands")
-
+		// kak try
+		io.WriteString(stdin, "try %{ ")
+		io.WriteString(stdin, "eval ")
 		if kctx.Buffer.Name != "" {
-			io.WriteString(stdin, fmt.Sprintf(" -buffer %s", kctx.Buffer.Name))
+			io.WriteString(stdin, fmt.Sprintf("-buffer %s ", kctx.Buffer.Name))
 		} else if kctx.Client.Name != "" {
-			io.WriteString(stdin, fmt.Sprintf(" -try-client %s", kctx.Client.Name))
+			io.WriteString(stdin, fmt.Sprintf("-try-client %s ", kctx.Client.Name))
 		}
 
-		io.WriteString(stdin, fmt.Sprintf(" %s", command))
+		io.WriteString(stdin, fmt.Sprintf("%s ", kakCommand))
+		io.WriteString(stdin, "}")
+
+		// kak catch
+		io.WriteString(stdin, "catch %{ ")
+		io.WriteString(stdin, "echo -debug kks: %val{error} ")
+		io.WriteString(stdin, "\n")
+		if errOutFile != nil {
+			// write a prefixed error to tmp file so that we can parse it in runner and decide what to do
+			io.WriteString(stdin, fmt.Sprintf("echo -to-file %s __kak_error__ %%val{error} ", errOutFile.Name()))
+			io.WriteString(stdin, "\n")
+		}
+		io.WriteString(stdin, "eval ")
+		if kctx.Buffer.Name != "" {
+			io.WriteString(stdin, fmt.Sprintf("-buffer %s ", kctx.Buffer.Name))
+		} else if kctx.Client.Name != "" {
+			io.WriteString(stdin, fmt.Sprintf("-try-client %s ", kctx.Client.Name))
+		}
+		io.WriteString(stdin, "%{ echo -markup {Error}kks: %val{error} } ")
+		io.WriteString(stdin, "}")
 
 		stdin.Close()
 	}()
@@ -35,5 +54,6 @@ func Send(kctx *Context, command string) error {
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
